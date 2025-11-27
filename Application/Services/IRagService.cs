077b41cb4015f1c80c5a3b51e09d801efd0a4a56
Trung <trunghoang220703@gmail.com>
2025-11-27 17:25:30 +0700

    commit

---
+++ Application/Services/IRagService.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Services;
+
+public interface IRagService
+{
+    // Bước 1: Truy xuất (Retrieval) - Tìm Context liên quan
+    Task<List<string>> RetrieveContextAsync(string query, int k = 3);
+    
+    // Bước 2: Tạo Phản hồi (Generation) - Tổng hợp câu trả lời
+    Task<string> GenerateAnswerAsync(string userQuery, List<string> context);
+}
---
+++ Domain/Entities/PgEmbedding.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities;
+
+public class PgEmbedding
+{
+    public Guid Id { get; set; } 
+    public string Document { get; set; } // Nội dung chunk (Context)
+    public float[] Embedding { get; set; } // Vector (Mảng float)
+    public Guid CollectionId { get; set; }
+}
